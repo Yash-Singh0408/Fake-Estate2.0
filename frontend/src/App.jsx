41e5f6ee2d929e4045654480e50f044024fbd63f
Yash Singh <yashsinghofficial04@gmail.com>
2025-02-05 20:11:15 +0530

    responnsive home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import "./layout.scss";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Homepage from "./pages/homePage/Homepage.jsx";
+
 function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div className="layout">
+      <div className="nav">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/homePage/Homepage.jsx
@@ -0,0 +1,35 @@
+import "./homepage.scss";
+import SearchBar from "../../components/searchBar/SearchBar.jsx";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Buy Sell and Rent Your Dream Properties</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <SearchBar/>
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years Experience</h2>
+            </div>
+            <div className="box">
+              <h1>100+</h1>
+              <h2>Properties Sold</h2>
+            </div>
+            <div className="box">
+              <h1>50+</h1>
+              <h2>Agents</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imageContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
