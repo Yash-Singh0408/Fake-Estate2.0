fded82f15a7b2d31393b6f8d71690d5282c30ee3
Yash Singh <yashsinghofficial04@gmail.com>
2025-04-16 12:43:04 +0530

    backend auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure: true       //comment out in production
+        secure: true,
         maxAge: age,
       })
       .status(200)
